56163eb74f67c076f45aa00d89177b1e4b0cbb90
Nikolay Sokolov <cto@mineinvest.ru>
2022-05-11 06:40:46 +0300

    Post, Category models created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    title = models.CharField(max_length=100, blank=False, null=False, verbose_name='Title')
+    position = models.IntegerField(default=0, blank=False, null=False, verbose_name='Index position')
+
+
+class Post(models.Model):
+    class Meta:
+        db_table = 'posts'
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    title = models.CharField(max_length=255, blank=False, null=False, verbose_name='Title')
+    post_text = models.TextField(verbose_name='Text')
+    time_created = models.DateTimeField(auto_now_add=True, verbose_name='Add time')
